b05017b2943017c1767b73de21c90763fa43d3c2
heera9331 <71516269+heera9331@users.noreply.github.com>
2022-07-26 00:45:51 +0530

    Create jumpingGame2.java

---
+++ jumpingGame2.java
@@ -0,0 +1,33 @@
+// circle game/football game
+
+import java.util.Scanner;
+class Main {
+    static int jumpCount(String str) {
+        int count=0;
+        int n = str.length()-1;
+        int l = str.length();
+
+        count = n/3;
+        if(n%3 == 0) {
+            return count;
+        } else {
+            n = l - l%3;
+            count += (n/2)+1;
+            if(n%2 == 0) {
+                count += count+n/2;
+                 
+            } else {
+                n = l - l%2;
+                count += n+1;
+                return count;   
+            }
+        }
+        return count;
+    }
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+      
+        String str=sc.next();
+        System.out.println(jumpCount(str));
+    }
+}
