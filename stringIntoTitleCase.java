93539d50126db6bec2139e911c09e95affccda91
heera9331 <71516269+heera9331@users.noreply.github.com>
2022-07-25 11:42:14 +0530

    Create stringIntoTitleCase.java

---
+++ stringIntoTitleCase.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+
+        String str = "heera";
+
+        System.out.println(str.substring(0,1).toUpperCase()+str.substring(1));
+    }
+}
