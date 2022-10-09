ece945b1b8f8e8667c81a3170e6263f88bbd266c
Heera Singh Lodhi <71516269+heera9331@users.noreply.github.com>
2022-10-10 02:01:04 +0530

    Create 21_intersectionOfTwoLinkedList.java

---
+++ 21_intersectionOfTwoLinkedList.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ 
+ 
+ Input: intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+Output: Intersected at '2'
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while (a != b) {
+          a = a == null ? headB : a.next;
+          b = b == null ? headA : b.next;
+        }
+
+        return a;
+    }
+}
